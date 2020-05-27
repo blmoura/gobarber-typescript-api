15eb1be08424a93e45ce6ac97478d4d1ab538d6e
Bruno Moura <brunomoura.blm@gmail.com>
2020-05-27 16:51:34 -0300

    Usando Controllers Nessa etapa, estou tirando todo o código que estava contigo nas rotas e passando pra uma classe controller. Dessa forma, estamos enxugando toda a responsabilidade que estava nas rotas pra um controller.

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
 
-import CreateAppointmentService from '@modules/appointments/services/CreateApoointmentService';
+import AppointmentController from '@modules/appointments/infra/http/controller/AppointmentsController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middleware/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
@@ -16,18 +15,6 @@ appointmentsRouter.use(ensureAuthenticated);
 //   return response.json(appointments);
 // });
 
-appointmentsRouter.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-  return response.json(appointment);
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
