7447c35dbfac988ffcbcea7ec53edbb2f831da3e
Bruno Moura <brunomoura.blm@gmail.com>
2020-05-28 16:21:17 -0300

    Criando o primeiro teste Nessa etapa, estou criando o primeiro teste que é a criação de Appointments. Fiz a criação de um repositorio fake, que se trata basicamente de, ao inves de ter uma conexao com o banco pra testar nosso service, salvaremos os dados em memoria. Então foi criado um repositorio fake, que implementa a interface de repositorio e sobrescrevemos o método, usando a mesma logica, pra testar se esta salvando os dados corretamente e retornando o esperado.

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,38 @@
+import { uuid } from 'uuidv4';
+
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+
+    return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+    // Essa linha acima faz a mesma coisa que essas linhas abaixo, estamos preenchendo
+    // o appointment com os dados que vem como parametro
+    // appointment.id = uuid();
+    // appointment.date = date;
+    // appointment.provider_id = provider_id;
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
